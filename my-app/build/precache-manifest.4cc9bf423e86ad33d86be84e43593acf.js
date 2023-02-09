323849569641114c7c22604af34873ae25c6bd90
Riya <riyachaudhary1555@gmail.com>
2023-02-09 13:49:05 +0530

    initial commit

---
+++ my-app/build/precache-manifest.4cc9bf423e86ad33d86be84e43593acf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89b166db8aacc893199b33ffe6bea80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "374b56a3f6ebbbb65c2a",
+    "url": "/static/css/main.7024a767.chunk.css"
+  },
+  {
+    "revision": "36044de378f7f853024e",
+    "url": "/static/js/2.04bd51a9.chunk.js"
+  },
+  {
+    "revision": "be9132a2347196dac6535e64c68b302b",
+    "url": "/static/js/2.04bd51a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374b56a3f6ebbbb65c2a",
+    "url": "/static/js/main.8519afc0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "7281ad742aa0c44cb84281b3d192dc92",
+    "url": "/static/media/430291.7281ad74.jpg"
+  },
+  {
+    "revision": "004db09fa5f8015cfc7bc250526438c9",
+    "url": "/static/media/Polygon1.004db09f.svg"
+  }
+]);
